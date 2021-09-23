ba1323a115d04c354578d080496b2b3e91ea6ed5
vidyasagar gaikwad <vidyasagargaikwad7128@gmail.com>
2021-09-23 22:56:41 +0530

    Added : Binary Search Word

--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -7,6 +7,15 @@ namespace DataStructures
         static void Main(string[] args)
         {
             Console.WriteLine("Data Structure!");
+            DataStructure dataStructure = new DataStructure();
+            string[] words = { "decs", "read", "logic", "search" };
+            string data = "search";
+            int result = dataStructure.FindWord(words, data);
+            if (result == -1)
+                Console.WriteLine("Word not present");
+
+            else
+                Console.WriteLine("Word is found at " + result);
         }
     }
 }
